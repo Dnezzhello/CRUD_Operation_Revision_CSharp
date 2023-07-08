3afcdd8fb5bc2af04a160315e216a58758fc2752
Dnezzhello <spxphot77@gmail.com>
2023-07-08 15:37:53 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace examRevision
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rdBtnMale = new System.Windows.Forms.RadioButton();
+            this.rdBtnFemale = new System.Windows.Forms.RadioButton();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ລະຫັດ: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ຊື່: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ນາມສະກຸນ: ";
+            // 
+            // rdBtnMale
+            // 
+            this.rdBtnMale.AutoSize = true;
+            this.rdBtnMale.Location = new System.Drawing.Point(126, 170);
+            this.rdBtnMale.Name = "rdBtnMale";
+            this.rdBtnMale.Size = new System.Drawing.Size(60, 29);
+            this.rdBtnMale.TabIndex = 3;
+            this.rdBtnMale.TabStop = true;
+            this.rdBtnMale.Text = "ຊາຍ";
+            this.rdBtnMale.UseVisualStyleBackColor = true;
+            // 
+            // rdBtnFemale
+            // 
+            this.rdBtnFemale.AutoSize = true;
+            this.rdBtnFemale.Location = new System.Drawing.Point(192, 170);
+            this.rdBtnFemale.Name = "rdBtnFemale";
+            this.rdBtnFemale.Size = new System.Drawing.Size(50, 29);
+            this.rdBtnFemale.TabIndex = 4;
+            this.rdBtnFemale.TabStop = true;
+            this.rdBtnFemale.Text = "ຍິງ";
+            this.rdBtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(126, 27);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(225, 34);
+            this.txtID.TabIndex = 5;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(126, 74);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(225, 34);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(126, 124);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(225, 34);
+            this.txtSurname.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ເພດ:​ ";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(51, 26);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(303, 42);
+            this.btnConnect.TabIndex = 9;
+            this.btnConnect.Text = "ເຊື່ອມຕໍ່ຖານຂໍ້ມູນ";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(51, 84);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(303, 42);
+            this.btnInsert.TabIndex = 10;
+            this.btnInsert.Text = "ເພີ່ມຂໍ້ມູນ";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(51, 143);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(303, 42);
+            this.btnEdit.TabIndex = 11;
+            this.btnEdit.Text = "ແກ້ໄຂຂໍ້ມູນ";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(51, 200);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(303, 42);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "ລຶບຂໍ້ມູນ";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtSurname);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.txtID);
+            this.groupBox1.Controls.Add(this.rdBtnFemale);
+            this.groupBox1.Controls.Add(this.rdBtnMale);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Phetsarath OT", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(31, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(416, 227);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ບ່ອນຕື່ມຂໍ້ມູນ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnShowAll);
+            this.groupBox2.Controls.Add(this.btnDelete);
+            this.groupBox2.Controls.Add(this.btnEdit);
+            this.groupBox2.Controls.Add(this.btnInsert);
+            this.groupBox2.Controls.Add(this.btnConnect);
+            this.groupBox2.Font = new System.Drawing.Font("Phetsarath OT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(28, 275);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(418, 309);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ປຸ່ມ";
+            // 
+            // dataGridView
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Phetsarath OT", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Phetsarath OT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView.Location = new System.Drawing.Point(464, 84);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(566, 499);
+            this.dataGridView.TabIndex = 15;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            // 
+            // btnFind
+            // 
+            this.btnFind.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnFind.Font = new System.Drawing.Font("Phetsarath OT", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFind.ForeColor = System.Drawing.Color.Cornsilk;
+            this.btnFind.Location = new System.Drawing.Point(819, 36);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(211, 42);
+            this.btnFind.TabIndex = 16;
+            this.btnFind.Text = "ຄົ້ນຫາຜ່ານລະຫັດ";
+            this.btnFind.UseVisualStyleBackColor = false;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Phetsarath OT", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(464, 42);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(349, 34);
+            this.txtSearch.TabIndex = 17;
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnShowAll.Location = new System.Drawing.Point(51, 257);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(303, 42);
+            this.btnShowAll.TabIndex = 13;
+            this.btnShowAll.Text = "ສະແດງຂໍ້ມູນທັງໝົດ";
+            this.btnShowAll.UseVisualStyleBackColor = false;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 606);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rdBtnMale;
+        private System.Windows.Forms.RadioButton rdBtnFemale;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnShowAll;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.CodeDom;
+
+namespace examRevision
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlDataAdapter da;
+        SqlCommand cmd;
+        DataSet ds;
+        DataTable dt;
+        string strConn = @"Data Source=localhost;Initial Catalog=dbcshap2023;Integrated Security=True";
+        string gender;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showData() 
+        {
+            dt = new DataTable();
+            da = new SqlDataAdapter("SELECT * FROM tbEmployee", conn);
+            da.Fill(dt);
+            dataGridView.DataSource = dt;
+
+            dataGridView.Columns[0].HeaderText = "ລະຫັດພະນັກງານ";
+            dataGridView.Columns[1].HeaderText = "ຊື່";
+            dataGridView.Columns[2].HeaderText = "ນາມສະກຸນ";
+            dataGridView.Columns[3].HeaderText = "ເພດ";
+
+            conn.Close();
+        }
+        private void clearData()
+        {
+            txtID.Clear();
+            txtName.Clear();
+            txtSurname.Clear();
+            if (rdBtnFemale.Checked)
+            {
+                rdBtnFemale.Checked = false;
+            } else
+            {
+                rdBtnMale.Checked = false;
+            }
+        }
+
+        private string getGender()
+        {
+            if (rdBtnFemale.Checked)
+            {
+                return "ຍິງ";
+            } else
+            {
+                return "ຊາຍ";
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(strConn);
+            conn.Open();
+            MessageBox.Show("Connection Succeeded!");
+            showData();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string insertSql;
+            conn.Open();
+
+            gender = getGender();
+
+            insertSql = $"INSERT INTO tbEmployee VALUES ({Convert.ToInt32(txtID.Text)}, N'{txtName.Text}', " +
+                $"N'{txtSurname.Text}', N'{gender}')";
+            cmd = new SqlCommand(insertSql, conn);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("ການເພີ່ມຂໍ້ມູນສຳເລັດ");
+            showData();
+            clearData();
+            
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            string updateSql;
+            conn.Open();
+
+            gender = getGender();
+
+            updateSql = $"UPDATE tbEmployee SET ename=N'{txtName.Text}', " +
+                $"esurname = N'{txtSurname.Text}', gender=N'{gender}' WHERE eid LIKE {txtID.Text}";
+            cmd = new SqlCommand(updateSql, conn);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("ການແກ້ໄຂຂໍ້ມູນສຳເລັດ");
+            showData();
+            clearData();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string deleteSql;
+            conn.Open();
+
+            deleteSql = $"DELETE FROM tbEmployee WHERE eid LIKE {txtID.Text}";
+            cmd = new SqlCommand(deleteSql, conn);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("ການລົບຂໍ້ມູນສຳເລັດ");
+            showData();
+            clearData();
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtID.Text = dataGridView.Rows[dataGridView.CurrentRow.Index].Cells[0].Value.ToString();
+            txtName.Text = dataGridView.Rows[dataGridView.CurrentRow.Index].Cells[1].Value.ToString();
+            txtSurname.Text = dataGridView.Rows[dataGridView.CurrentRow.Index].Cells[2].Value.ToString();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            string querySql;
+            conn.Open();
+            querySql = $"SELECT * FROM tbEmployee WHERE eid LIKE {txtSearch.Text}";
+            cmd = new SqlCommand(querySql, conn);
+            cmd.ExecuteNonQuery();
+
+            dt = new DataTable();
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            dataGridView.DataSource = dt;
+
+            txtID.Text = dt.ToString();
+            txtName.Text = dt.ToString();
+            txtSurname.Text = dt.ToString();
+
+            conn.Close();
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            showData();
+        }
+    }
+}
